080a7570558760834f899dc97faab587d386b0f6
Jeremy Epstein <jazepstein@gmail.com>
2021-01-11 13:29:27 +1100

    switch from google maps to leaflet/openstreetmap

--- static/js/location-map.js
+++ static/js/location-map.js
@@ -6,36 +6,47 @@ worldtrip.location_map = function() {
     var icon_url = null;
 
     function add_location_marker(k, loc) {
-        var marker = new google.maps.Marker({
-            icon: icon_url,
-            position: new google.maps.LatLng(loc.lat, loc.lon),
-            map: map,
-            title: loc.title
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-            window.location.href = loc.url;
-        });
+        var marker = L.marker(
+            [loc.lat, loc.lon], {
+                icon: L.icon({
+                    iconUrl: icon_url,
+                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+                    iconSize: [25, 41],
+                    iconAnchor: [12, 41],
+                    popupAnchor: [1, -34],
+                    shadowSize: [41, 41]
+                }),
+                title: loc.title})
+            .addTo(map)
+            .on('click', function(e) {
+                window.location.href = loc.url;
+            });
     }
-    
+
     return {
         init: function(jq) {
             $ = jq;
 
-            var mapOptions = {
-                zoom: 1,
-                center: new google.maps.LatLng(40.0, 0.0)
-            }
+            map = L.map('nodemap').setView([30.0, 0.0], 1);
 
-            map = new google.maps.Map(document.getElementById('nodemap'), mapOptions);
+            if (typeof mapbox_api_key !== 'undefined') {
+                L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    maxZoom: 18,
+                    id: 'mapbox/streets-v11',
+                    tileSize: 512,
+                    zoomOffset: -1,
+                    accessToken: mapbox_api_key
+                }).addTo(map);
 
-            if (typeof countries !== 'undefined') {
-                icon_url = 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png';
-                $.each(countries, add_location_marker);
-            }
-            if (typeof cities !== 'undefined') {
-                icon_url = 'https://maps.google.com/mapfiles/ms/icons/red-dot.png';
-                $.each(cities, add_location_marker);
+                if (typeof countries !== 'undefined') {
+                    icon_url = 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png';
+                    $.each(countries, add_location_marker);
+                }
+                if (typeof cities !== 'undefined') {
+                    icon_url = 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png';
+                    $.each(cities, add_location_marker);
+                }
             }
         }
     }
